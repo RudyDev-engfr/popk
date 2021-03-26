4f6580f9a21bd5408e6ef34b611a3c5279f156fa
Pierre.C <78153758+C-Pierre@users.noreply.github.com>
2021-03-26 15:18:33 +0100

    menu

--- livreor.php
+++ livreor.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8">
   <title>Livre d'or</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./css/style.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous">
   <script src="https://kit.fontawesome.com/d93dff7d82.js"></script> 
