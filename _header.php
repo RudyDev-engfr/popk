2ba1b859f0b883e9c804ba9555e7870ae11e0937
Labielle Bryan <bryan.labielle@hotmail.com>
2021-03-26 09:26:45 +0100

    update

--- _header.php
+++ _header.php
@@ -1,36 +1,37 @@
-<header id="navbar">
-    <div class="brand">
-        <div class="nav-hours">
-            <p class="pMenu"><i class="fas fa-calendar-alt iFooter"></i>M-M-J-V-S</p> 
-            <p class="pMenu"><i class="fas fa-clock iFooter"></i>15H30/00H</p>
+<main id="main_nav">
+    <header id="navbar">
+        <div class="brand">
+            <div class="nav-hours">
+                <p class="pMenu"><i class="fas fa-calendar-alt iFooter"></i>M-M-J-V-S</p> 
+                <p class="pMenu"><i class="fas fa-clock iFooter"></i>15H30/00H</p>
+            </div>
+            <div class="logo">
+                <a class="aMenu" href="index.php">Logo</a>
+            </div>
         </div>
-        <div class="logo">
-            <a class="aMenu" href="index.php">Logo</a>
+        <div id="nav-mobile">
+            <i class="fas fa-bars iFooter"></i>
         </div>
-    </div>
-    <div id="nav-mobile">
-        <i class="fas fa-bars iFooter"></i>
-    </div>
-    <nav>
-        <ul class="nav-list">
-            <li class="liMenu">
-                <a class="aMenu" href="#!">PopK<i class="fas fa-home"></i></a>
-                <ul class="nav-dropdown">
-                    <li class="liMenu2"><a class="aMenu" href="popk.php">Le cyber-café<i class="fas fa-laptop-house"></i></a></li>
-                    <li class="liMenu2"><a class="aMenu" href="popk.php#ancrePop1">Infos pratiques<i class="fas fa-info-circle"></i></a></li>
-                    <li class="liMenu2"><a class="aMenu" href="popk.php#ancrePop2">Contactez-nous<i class="fas fa-address-book"></i></a></li>
-                </ul>
-            </li>
-            <li class="liMenu"><a class="aMenu" href="carte.php">PopKarte<i class="fas fa-mug-hot"></i></a></li>
-            <li class="liMenu">
-                <a class="aMenu" href="#!">Events<i class="fas fa-calendar-alt"></i></a>
-                <ul class="nav-dropdown">
-                    <li class="liMenu2"><a class="aMenu" href="évènements.php">À venir<i class="fas fa-calendar-check"></i></a></li>
-                    <li class="liMenu2"><a class="aMenu" href="évènements.php">Hall of Fame<i class="fas fa-trophy"></i></a></li>
-                </ul>
-            </li>
-            <li class="liMenu"><a class="aMenu" href="livreor.php">Livre d'or<i class="fas fa-book"></i></a></li>
-        </ul>
-    </nav>
-</header>
-
+        <nav>
+            <ul class="nav-list">
+                <li class="liMenu">
+                    <a class="aMenu" href="#!">PopK<i class="fas fa-home"></i></a>
+                    <ul class="nav-dropdown">
+                        <li class="liMenu2"><a class="aMenu" href="popk.php">Le cyber-café<i class="fas fa-laptop-house"></i></a></li>
+                        <li class="liMenu2"><a class="aMenu" href="popk.php#ancrePop1">Infos pratiques<i class="fas fa-info-circle"></i></a></li>
+                        <li class="liMenu2"><a class="aMenu" href="popk.php#ancrePop2">Contactez-nous<i class="fas fa-address-book"></i></a></li>
+                    </ul>
+                </li>
+                <li class="liMenu"><a class="aMenu" href="carte.php">PopKarte<i class="fas fa-mug-hot"></i></a></li>
+                <li class="liMenu">
+                    <a class="aMenu" href="#!">Events<i class="fas fa-calendar-alt"></i></a>
+                    <ul class="nav-dropdown">
+                        <li class="liMenu2"><a class="aMenu" href="évènements.php">À venir<i class="fas fa-calendar-check"></i></a></li>
+                        <li class="liMenu2"><a class="aMenu" href="évènements.php">Hall of Fame<i class="fas fa-trophy"></i></a></li>
+                    </ul>
+                </li>
+                <li class="liMenu"><a class="aMenu" href="livreor.php">Livre d'or<i class="fas fa-book"></i></a></li>
+            </ul>
+        </nav>
+    </header>
+</main>
